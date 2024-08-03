b1d63faa1947cdc4537d7aa5fa9ca9a0249faf14
Hong-Kuan Wu <maxam.cs05@g2.nctu.edu.tw>
2024-08-03 10:21:56 +0800

    feat: day1 completed

--- 001/main.py
+++ 001/main.py
@@ -1,1 +1,11 @@
+print("Welcome to the Band Name Generator.")
 
+# ask brand name
+city_name = input("What's the name of the city you grew up in?\n")
+
+# ask pet name
+pet_name = input("What's your pet's name?\n")
+
+# print brand name
+brand_name = city_name + " " + pet_name
+print("Your band name could be " + brand_name)
